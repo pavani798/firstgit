94033d4fb9ea69bddd1505f8aaa19cd5109863de
Admin <Admin@DESKTOP-US8N5GS>
2022-07-01 20:45:02 +0530

    my first git

---
+++ src/gitdemo/gitondemo.java
@@ -0,0 +1,12 @@
+package gitdemo;
+
+public class gitondemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("my first git program");
+
+	}
+
+}
